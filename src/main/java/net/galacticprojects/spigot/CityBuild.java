23095ff0c9b8f5cb2fdd4cdaeabdb1467c0453a7
TntTastisch <tnttastisch@gmail.com>
2022-10-13 00:10:10 +0200

    Created projects

---
+++ src/main/java/net/galacticprojects/spigot/CityBuild.java
@@ -0,0 +1,17 @@
+package net.galacticprojects.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+
+public class CityBuild extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
